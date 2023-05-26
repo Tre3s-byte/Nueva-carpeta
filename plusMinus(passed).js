32808dfc440b753ee244479860ed316ec2deb6bd
Tre3s-Byte <malmar1701@gmail.com>
2023-05-26 13:02:29 -0500

    code pass

---
+++ plusMinus(passed).js
@@ -0,0 +1,27 @@
+function plusMinus(arr) {
+    let positives = 0;
+    let equalToZero = 0;
+    let negatives = 0;
+    let length = arr.length;
+    for (let i = 0; i < length; i++) {
+      if (arr[i] > 0) {
+        positives += 1;
+      } else if (arr[i] == 0) {
+        equalToZero += 1;
+      } else if (arr[i] < 0) {
+        negatives += 1;
+      } else {
+        return "Not a number";
+      }
+    }
+    let positiveRatio = (positives / length).toFixed(6);    
+    let equalToZeroRatio = (equalToZero / length).toFixed(6);
+    let negativesRatio = (negatives / length).toFixed(6);
+  
+    console.log(positiveRatio) 
+    console.log(negativesRatio)
+    console.log(equalToZeroRatio)
+    
+}
+
+plusMinus([-4, 3, -9, 0, 4, 1])
